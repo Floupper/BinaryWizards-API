a3e5af27715e683085d5c37365c65c732e01d333
Sylvain <sylvainschaefer@gmail.com>
2024-11-13 09:29:17 +0100

    fix: add validation for routes

--- src/Validation/quiz.ts
+++ src/Validation/quiz.ts
@@ -1,4 +1,4 @@
-import { object, string, number, optional, refine, size } from 'superstruct';
+import { object, string, number, optional, refine, size, pattern } from 'superstruct';
 
 const Category = optional(refine(number(), 'Category', (value) => { // Category must be between 9 and 32 because Open Trivia Database uses a range of categories from 9 to 32.
     return value >= 9 && value <= 32;
@@ -9,3 +9,9 @@ export const QuizCreationData = object({
     difficulty: optional(string()),
     amount: size(number(), 1, 50),
 });
+
+
+export const UUID = pattern(
+    string(),
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+);
