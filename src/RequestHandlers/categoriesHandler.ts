762bca80ba4133bde99b626f97bdae9ccbf484d5
Sylvain <sylvainschaefer@gmail.com>
2024-11-14 11:49:43 +0100

    fix: retrieving categories by the open db api

--- src/RequestHandlers/categoriesHandler.ts
+++ src/RequestHandlers/categoriesHandler.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { get_all_categories } from '../Repositories/categoriesRepository';
+import axios from 'axios';
 
 
 export async function get_all(req: Request, res: Response) {
-    const categories = await get_all_categories();
+    const response = await axios.get('https://opentdb.com/api_category.php');
 
-    if (categories)
-        res.json(categories);
+    if (response.data.trivia_categories)
+        res.json(response.data.trivia_categories);
     else
         res.status(404).json({ message: 'Error while finding categories.' });
 }
