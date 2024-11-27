386ac112f1f7636645812ce0dcbf0f735c864056
Sylvain <sylvainschaefer@gmail.com>
2024-11-27 10:38:53 +0100

    fix: removing console.log

--- src/RequestHandlers/usersHandler.ts
+++ src/RequestHandlers/usersHandler.ts
@@ -101,7 +101,6 @@ export const get_quizzes = async (req: Request, res: Response) => {
 
     try {
         const quizzes = await get_user_quizzes(user_id);
-        console.log(quizzes);
 
         // Build response
         const quizzesWithStats = quizzes.map(quiz => {
