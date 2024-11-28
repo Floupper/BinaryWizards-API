36479cf647f20f9e17236724b0922c1456dcecd2
Sylvain <sylvainschaefer@gmail.com>
2024-11-28 09:52:47 +0100

    fix: removing french commentaries

--- src/Middlewares/logsMiddleware.ts
+++ src/Middlewares/logsMiddleware.ts
@@ -1,19 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
-// Middleware de logging
+// Logging middleware
 export function requestLogger(req: Request, res: Response, next: NextFunction) {
-    // Capturez la date de début de la requête
     const startTime = Date.now();
 
-    // Enregistrez les informations sur la requête reçue
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
 
-    // Attendez la fin de la requête pour enregistrer les informations sur la réponse
     res.on('finish', () => {
         const elapsedTime = Date.now() - startTime;
         console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl} - Status: ${res.statusCode} - Temps: ${elapsedTime} ms`);
     });
 
-    // Passez au middleware suivant
     next();
 }
