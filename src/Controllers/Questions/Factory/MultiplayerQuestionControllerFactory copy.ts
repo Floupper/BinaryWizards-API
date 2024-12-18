86ba8ec0c3d69f7acda8facee1ec2929c58321b5
Sylvain <sylvainschaefer@gmail.com>
2024-12-18 10:44:58 +0100

    feat: adding ScrumGame mode controller

--- src/Controllers/Questions/Factory/MultiplayerQuestionControllerFactory copy.ts
+++ src/Controllers/Questions/Factory/MultiplayerQuestionControllerFactory copy.ts
@@ -1,6 +1,7 @@
 import { MultiplayerQuestionControllerInterface } from '../../../Interfaces/MultiplayerQuestionControllerInterface';
 import { Dependencies } from '../../../Interfaces/Dependencies';
 import { TeamQuestionController } from '../TeamQuestionController';
+import { ScrumQuestionController } from '../ScrumQuestionController';
 
 export class MultiplayerQuestionControllerFactory {
     static getController(mode: string, dependencies: Dependencies): MultiplayerQuestionControllerInterface {
@@ -10,6 +11,11 @@ export class MultiplayerQuestionControllerFactory {
                     throw new Error('Socket.IO server instance is required for team question controller');
                 }
                 return new TeamQuestionController(dependencies.io);
+            case 'scrum':
+                if (!dependencies.io) {
+                    throw new Error('Socket.IO server instance is required for scrum question controller');
+                }
+                return new ScrumQuestionController(dependencies.io);
             default:
                 throw new Error('Invalid game mode');
         }
