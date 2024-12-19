84ce87770720aca7f4b423f68aefb76276097491
Sylvain <sylvainschaefer@gmail.com>
2024-12-19 10:19:30 +0100

    fix: gestion of the errors in singleplayer game modes

--- src/Controllers/Games/TimeGameController.ts
+++ src/Controllers/Games/TimeGameController.ts
@@ -1,21 +1,22 @@
 import { GameControllerInterface } from '../../Interfaces/GameControllerInterface';
 import { persist_game, update_game_status } from '../../Repositories/gamesRepository';
+import { SocketError } from '../../Sockets/SocketError';
 
 export class TimeGameController implements GameControllerInterface {
     async init(quiz_id: string, user_id: string | null, data: any) {
         const { difficulty_level } = data;
         if (!difficulty_level) {
-            throw new Error('difficulty_level is required for time mode');
+            throw new SocketError('difficulty_level is required for time mode');
         }
         return await this.init_time_game(quiz_id, user_id, difficulty_level);
     }
 
     async join(game: any, user: any, data: any) {
-        throw new Error(`The game mode ${game.mode} does not support the join game action.`);
+        throw new SocketError(`The game mode ${game.mode} does not support the join game action.`);
     }
 
     async start(game: any) {
-        throw new Error(`The game mode ${game.mode} does not support the start game action.`);
+        throw new SocketError(`The game mode ${game.mode} does not support the start game action.`);
     }
 
     async init_time_game(quiz_id: string, user_id: string | null, difficulty_level: string) {
