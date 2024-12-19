38f2ea61caa734c98221206b758879455f6927f7
Sylvain <sylvainschaefer@gmail.com>
2024-12-19 14:25:00 +0100

    fix: notifying other players that someone joins the game

--- src/Controllers/Games/TeamGameController.ts
+++ src/Controllers/Games/TeamGameController.ts
@@ -1,6 +1,6 @@
 import { Server, Socket } from 'socket.io';
 import { GameControllerInterface } from '../../Interfaces/GameControllerInterface';
-import { get_players_in_game, get_teams_in_game, is_team_player, persist_game, update_game_status } from '../../Repositories/gamesRepository';
+import { get_players_in_game, get_teams_in_game, get_teams_players_in_game, is_team_player, persist_game, update_game_status } from '../../Repositories/gamesRepository';
 import { assign_player_to_team, find_team, init_team_for_game } from '../../Repositories/teamsRepository';
 import { Games } from '@prisma/client';
 import { SocketError } from '../../Sockets/SocketError';
@@ -53,7 +53,7 @@ export class TeamGameController implements GameControllerInterface {
         await assign_player_to_team(team.team_id, user.user_id);
 
 
-        const teams = (await get_players_in_game(game.game_id));
+        const teams = (await get_teams_players_in_game(game.game_id));
         // Join the corresponding Socket.IO room for the game
         socket.join(game.game_id);
 
