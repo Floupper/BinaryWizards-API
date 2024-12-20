59b58bc264e5e708e3db1c89a42d67378a5304fe
Sylvain <sylvainschaefer@gmail.com>
2024-12-20 10:36:46 +0100

    fix: translating into english

--- src/Helpers/uploadsHelper.ts
+++ src/Helpers/uploadsHelper.ts
@@ -3,23 +3,19 @@ import path from 'path';
 import { Request } from 'express';
 import fs from 'fs';
 
-// Types de fichiers autorisés
 export const FILE_TYPES = {
     image: /jpeg|jpg|png|gif/,
     audio: /mp3|wav|ogg/
 };
 
-// Fonction pour générer un nom de fichier unique
 export const generateUniqueFilename = (originalName: string): string => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     return uniqueSuffix + path.extname(originalName);
 };
 
-// Fonction pour configurer le stockage
 export const configureStorage = (type: 'image' | 'audio'): StorageEngine => {
     const destination = path.join(__dirname, '../../public/uploads', `${type}s`);
 
-    // Assurez-vous que le dossier de destination existe
     if (!fs.existsSync(destination)) {
         fs.mkdirSync(destination, { recursive: true });
     }
@@ -34,7 +30,6 @@ export const configureStorage = (type: 'image' | 'audio'): StorageEngine => {
     });
 };
 
-// Fonction pour créer un filtre de fichiers
 export const createFileFilter = (type: 'image' | 'audio') => {
     return (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
         const extname = FILE_TYPES[type].test(path.extname(file.originalname).toLowerCase());
@@ -43,12 +38,11 @@ export const createFileFilter = (type: 'image' | 'audio') => {
         if (mimetype && extname) {
             return cb(null, true);
         } else {
-            cb(new Error(`Seuls les fichiers ${type}s sont autorisés.`));
+            cb(new Error(`Only ${type}s files are authorized.`));
         }
     };
 };
 
-// Fonction pour créer une instance de Multer
 export const createMulterInstance = (type: 'image' | 'audio', sizeLimit: number) => {
     const storage = configureStorage(type);
     const fileFilter = createFileFilter(type);
